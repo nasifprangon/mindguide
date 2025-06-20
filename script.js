ff3a0bd67fbce4c9d8819b0cba518eed0e8153ff
Nasif Fahmid Prangon <nasifprangon@gmail.com>
2025-06-20 11:32:36 -0400

    Update script.js to use live Render backend

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ async function sendPrompt() {
   responseBox.innerText = "Thinking...";
 
   try {
-    const response = await fetch('http://127.0.0.1:5000/chat', {
+    const response = await fetch('https://mindguide-backend.onrender.com/chat', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ prompt })
